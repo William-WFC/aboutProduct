510c1a1dd0e4f889e52c1d63832ed4aec1d8a9ed
王逢超 <wangfengchao@tbkt.cn>
2018-01-10 09:33:14 +0800

    * init

--- js/1.js
+++ js/1.js
@@ -1,5 +1,24 @@
 let a = 3;
-let b = 5;
+let b = 6;
 console.log(a, b);
 [a, b] = [b, a];
 console.log(a, b);
+let str = 'aaa_aa';
+let reg1 = /a+/y;
+console.log(reg1.exec(str));
+console.log(reg1.exec(str));
+console.log(reg1.exec(str));
+console.log(reg1.exec(str));
+console.log(reg1.exec(str));
+console.log(reg1.exec(str));
+console.log('test');
+if (a) {
+  console.log(a);
+} else if (b) {
+  console.log(b);
+} else {
+  console.log('a / b is false');
+}
+if (a) {
+  console.log(b);
+}
