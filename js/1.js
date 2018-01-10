d4723fa9f5abf93fd7cfa124d5bed352beca9c2d
王逢超 <wangfengchao@tbkt.cn>
2018-01-10 18:15:50 +0800

    * fix .js

--- js/1.js
+++ js/1.js
@@ -6,19 +6,11 @@ console.log(a, b);
 let str = 'aaa_aa';
 let reg1 = /a+/y;
 console.log(reg1.exec(str));
-console.log(reg1.exec(str));
-console.log(reg1.exec(str));
-console.log(reg1.exec(str));
-console.log(reg1.exec(str));
-console.log(reg1.exec(str));
-console.log('test');
-if (a) {
-  console.log(a);
-} else if (b) {
-  console.log(b);
-} else {
-  console.log('a / b is false');
+
+let arr = [1, 3, 5];
+for(let i of arr) {
+  console.log(i);
 }
-if (a) {
-  console.log(b);
+for (let i = 1; i < 1e7; i * 10) {
+  console.log(i);
 }
